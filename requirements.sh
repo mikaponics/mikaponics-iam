a2807f965004214abcae7aad06c0eda7464a9f4d
bartmika <bart@mikasoftware.com>
2019-09-09 22:33:57 -0400

    Added dependencies file.

---
+++ requirements.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+go get -u github.com/spf13/cobra/cobra    # Advanced console handling library.
+go get -u github.com/lib/pq               # Postgres DB Driver for Golang
+go get -u github.com/jmoiron/sqlx         # General purpose extensions to golang's database/sql
+go get -u golang.org/x/crypto/bcrypt      # Bycrypt Hashing Algorithm
+go get -u google.golang.org/grpc          # Google Remote Prodecure Call Library.
